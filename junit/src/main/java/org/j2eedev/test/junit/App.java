15d9cfeb23bb862dfb28de57ca138435dc1ba433
Umashankar <Umashankar@Umashankar-PC>
2015-09-06 13:58:49 +0530

    JUnit Samples Project

---
+++ junit/src/main/java/org/j2eedev/test/junit/App.java
@@ -0,0 +1,13 @@
+package org.j2eedev.test.junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
